7439c4810a27244407e0c56ed30bdf27e345dab3
johnty <johntywang@gmail.com>
2016-02-02 22:46:57 +0000

    moist.c update

--- moist.c
+++ moist.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
+#include <stdint.h>
 
 #define I2C_ADDR 0x20
 #define REG_RESET 0x06
 #define REG_VERS 0x07
 #define REG_TEMP 0x05
+#define REG_MOIST 0x00
 
 int main(int argc, char *argv[])
 {
@@ -24,7 +26,8 @@ int main(int argc, char *argv[])
    }
    else {
       printf("i2c init at 0x%04x OK, dev = %i\n", I2C_ADDR, dev);
-      int version = (int) wiringPiI2CReadReg8(dev, REG_VERS);
+      int version = (uint8_t) wiringPiI2CReadReg8(dev, REG_VERS);
+     // wiringPiI2CWriteReg8(dev, 0x01, 0x20); //change device addr
       printf("Moist version %i\n", version);
       delay(1000);
       wiringPiI2CWrite(dev, REG_RESET); //just write the "reset" addr to device 
@@ -33,9 +36,9 @@ int main(int argc, char *argv[])
    }
    while (1)
    {  
-      delayMicroseconds(1000*100);
-      int val = (int)wiringPiI2CReadReg16(dev,REG_TEMP);
-      printf("temp = %03i\n",val);
+      delayMicroseconds(1000*200);
+      int val0 = (uint16_t)wiringPiI2CReadReg16(dev,REG_TEMP);
+      printf("val = %03i\n", val0);
    }
    return 0;
 }
