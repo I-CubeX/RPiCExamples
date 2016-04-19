a546ac25c7e34c4111a1bcfa88fbc66ecd7daf3e
johnty <johntywang@gmail.com>
2016-04-19 22:05:34 +0100

    commandline test example

---
+++ test_cmd.sh
@@ -0,0 +1,32 @@
+
+
+
+#make the blinkM flash a bit...
+for ((i=0;i<4;i++)); do
+  i2cset -y 1 0x09 0x01 0x63 0xff 0x00 0x00 i # RED
+  sleep 0.1
+  i2cset -y 1 0x09 0x01 0x63 0x00 0xff 0x00 i # GREEN
+  sleep 0.1
+  i2cset -y 1 0x09 0x01 0x63 0x00 0x00 0xff i # BLUE
+  sleep 0.1
+done
+
+while true; do
+  VAL=`gpio -x mcp3004:100:0 aread 100`
+  BRI=`gpio -x mcp3004:100:0 aread 101`
+  A=`expr $VAL / 2 + 10`
+  if [ "$A" -gt 255 ];
+  then
+     A=255
+  fi
+  BRI=`expr $BRI / 4`
+  echo col = $A  bri = $BRI;
+  R=`expr $A \* $BRI / 255`
+  B=`expr 255 - $A`
+  B=`expr $B \* $BRI / 255`
+  G=0 
+  i2cset -y 1 0x09 0x01 0x6e $R $G $B i #set brightness according to sensor value
+  sleep 0.02
+  
+  
+done
